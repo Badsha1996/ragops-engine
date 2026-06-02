121337794b1664b08efc5d63ec43a248cb0a04ae
Badsha1996 <badshalaskar0@gmail.com>
2026-06-02 15:27:20 +0530

    ADDED actual bakend endpoint

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -108,8 +108,8 @@ export default function Dashboard() {
   });
   const [backendActive, setBackendActive] = useState(false);
 
-  const BACKEND_URL = "http://127.0.0.1:8000";
-  // const BACKEND_URL = "https://ragops-engine.onrender.com";
+  // const BACKEND_URL = "http://127.0.0.1:8000";
+  const BACKEND_URL = "https://ragops-engine.onrender.com";
 
   // Startup Hooks
   useEffect(() => {
